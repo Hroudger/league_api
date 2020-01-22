2e0bb05635338fdf36ddda85588ac75c473905b0
T3rmiXx <t3rmixx@DESKTOP-NJG0CAH.localdomain>
2020-01-22 12:06:02 +0100

    Projektskizze und erste Python Testdatei hinzugefügt

---
+++ riotapi.py
@@ -0,0 +1,7 @@
+import cassiopeia as cass
+import random
+
+
+cass.set_riot_api_key("RGAPI-c40a1cde-91d5-47b2-91b8-0bfe40a4e346")
+cass.set_default_region("EUW")
+summoner = cass.get_summoner(name="T3rmiXx")
