c4ada6ccfdfd53043537eacc57dc7947aeba652e
t3rmixx <wkaxYv37!?>
2020-01-29 12:59:45 +0100

    little script changes

--- scripts/riotapi.py
+++ scripts/riotapi.py
@@ -1,9 +1,7 @@
 import cassiopeia as cass
-import random
-
 
 cass.set_riot_api_key("RGAPI-4f2bbeb3-a75f-4708-b991-0075bd264982")
 cass.set_default_region("EUW")
-summoner = cass.get_summoner(name="T3rmiXx")
+summoner = cass.get_champion_rotations()
 
 print(summoner)
