0647ffe9628463141cbdb9cfe56881eeea15d377
Rüdiger Boll <r.boll@laudert.de>
2020-02-05 10:36:39 +0100

    [BE] implement basic code for methods

--- src/main/java/summoner/MatchHistory.java
+++ src/main/java/summoner/MatchHistory.java
@@ -1,4 +1,34 @@
 package summoner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MatchHistory {
+    private List<Match> matchList = new ArrayList<>();
+
+    public int getWinRate() {
+        return getWinRate("ranked");
+    }
+
+    public int getWinRate(String queue) {
+        return 0;
+    }
+
+    public int getAmount() {
+        return getAmount("ranked");
+    }
+
+    public int getAmount(String queue) {
+        return 0;
+    }
+
+    public int getPercentage(String queue) {
+        return 0;
+    }
+
+    public int getPercentage() {
+        return getPercentage("ranked");
+    }
+
+
 }
--- src/main/java/summoner/Summoner.java
+++ src/main/java/summoner/Summoner.java
@@ -1,4 +1,48 @@
 package summoner;
 
+import java.io.IOException;
+
 public class Summoner {
+
+    private MatchHistory matchHistory = new MatchHistory();
+    private String id;
+    private String name;
+    private String region;
+
+
+    public static void addSummoner(String name, String region) throws IOException {
+        String[] cmd = {
+                "python",
+                "MEIN SCRIPTPFAD",
+                name,
+                region
+        };
+        Runtime.getRuntime().exec(cmd);
+    }
+
+    public int getWinRate(String queue) {
+        return matchHistory.getWinRate(queue);
+    }
+
+    public int getWinRate() {
+        return matchHistory.getWinRate();
+    }
+
+    public int getAmount(String queue) {
+        return matchHistory.getAmount(queue);
+    }
+
+    public int getAmount() {
+        return matchHistory.getAmount();
+    }
+
+    public int getPercentage(String queue) {
+        return matchHistory.getPercentage(queue);
+    }
+
+    public int getPercentage() {
+        return matchHistory.getPercentage();
+    }
+
+
 }
